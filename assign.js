eb132f00c1001ea5b427514ad85cb6b7c95fcbd1
Mahidkarim <dataof098@gmail.com>
2025-08-28 18:35:58 +0600

    heart icon functionalities add

---
+++ assign.js
@@ -0,0 +1,117 @@
+
+// heart section /
+
+
+document.getElementById('heart-ash').addEventListener('click', function () {
+    const heartNum = parseInt(document.getElementById('heart-num').innerText)
+
+
+
+    const heartValue = heartNum + 1;
+
+    document.getElementById('heart-num').innerText = heartValue
+
+
+
+})
+document.getElementById('heart-ash2').addEventListener('click', function () {
+    const heartNum = parseInt(document.getElementById('heart-num').innerText)
+
+
+
+    const heartValue = heartNum + 1;
+
+    document.getElementById('heart-num').innerText = heartValue
+
+
+
+})
+document.getElementById('heart-ash3').addEventListener('click', function () {
+    const heartNum = parseInt(document.getElementById('heart-num').innerText)
+
+
+
+    const heartValue = heartNum + 1;
+
+    document.getElementById('heart-num').innerText = heartValue
+
+
+
+})
+document.getElementById('heart-ash4').addEventListener('click', function () {
+    const heartNum = parseInt(document.getElementById('heart-num').innerText)
+
+
+
+    const heartValue = heartNum + 1;
+
+    document.getElementById('heart-num').innerText = heartValue
+
+
+
+})
+document.getElementById('heart-ash5').addEventListener('click', function () {
+    const heartNum = parseInt(document.getElementById('heart-num').innerText)
+
+
+
+    const heartValue = heartNum + 1;
+
+    document.getElementById('heart-num').innerText = heartValue
+
+
+
+})
+document.getElementById('heart-ash6').addEventListener('click', function () {
+    const heartNum = parseInt(document.getElementById('heart-num').innerText)
+
+
+
+    const heartValue = heartNum + 1;
+
+    document.getElementById('heart-num').innerText = heartValue
+
+
+
+})
+document.getElementById('heart-ash7').addEventListener('click', function () {
+    const heartNum = parseInt(document.getElementById('heart-num').innerText)
+
+
+
+    const heartValue = heartNum + 1;
+
+    document.getElementById('heart-num').innerText = heartValue
+
+
+
+})
+document.getElementById('heart-ash8').addEventListener('click', function () {
+    const heartNum = parseInt(document.getElementById('heart-num').innerText)
+
+
+
+    const heartValue = heartNum + 1;
+
+    document.getElementById('heart-num').innerText = heartValue
+
+
+
+})
+document.getElementById('heart-ash9').addEventListener('click', function () {
+    const heartNum = parseInt(document.getElementById('heart-num').innerText)
+
+
+
+    const heartValue = heartNum + 1;
+
+    document.getElementById('heart-num').innerText = heartValue
+
+
+
+})
+
+
+
+
+
